44bdee69a855cbdd66b38632c195dad8934df938
Marlos Gabriel <mgsfarias99@gmail.com>
2025-09-17 19:22:52 -0300

    day02

---
+++ day02.py
@@ -0,0 +1,12 @@
+#Desafio: Permitir armazenar várias tarefas em uma lista.
+
+#1° pede as tarefas ao usuário:
+#tarefa1 = input("Digite a primeira tarefa: ")
+#tarefa2 = input("Digite a segunda tarefa: ")
+#tarefa3 = input("Digite a terceira tarefa: ")
+
+#2° Cria variavel lista com todas tarefas armazenadas:
+#lista_tarefas = [tarefa1, tarefa2, tarefa3]
+
+#3° lista as tarefas:
+#print(lista_tarefas)
